aac949769faf7da11a38d0acb634ee6384fb6f89
Elsie <yueyili128@gmail.com>
2026-01-17 22:55:15 -0500

    Add data loading functionality for MNIST dataset

---
+++ dataset.py
@@ -0,0 +1,20 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(batch_size=64):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    train_dataset = datasets.MNIST(
+        root="./data", train=True, download=True, transform=transform
+    )
+    test_dataset = datasets.MNIST(
+        root="./data", train=False, download=True, transform=transform
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    return train_loader, test_loader
